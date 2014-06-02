042eea53b80bc665e216dc6b76c32f321658e3f7
Dave Risney <dave@deletethis.net>
2014-06-01 21:18:24 -0700

    Update build.ps1 to make zip (fix #3)

--- build.ps1
+++ build.ps1
@@ -5,3 +5,6 @@ Copy-Item -Force LaunchAppxPackage/Release/LaunchAppxPackage.pdb bin
 Copy-Item -Force ExtractFromAppx/bin/Release/ExtractFromAppx.exe bin
 Copy-Item -Force ExtractFromAppx/bin/Release/ExtractFromAppx.pdb bin
 Copy-Item -Force Script/*.ps1 bin
+pushd bin
+zip ..\AppxUtilities.zip *
+popd
