9707108cd54f99fcb01ea24bbf65771e17a1d7e5
Dave Risney <dave@deletethis.net>
2014-06-22 18:32:45 -0700

    Add package execution state to Get-ProcessAppxPackage (Fix #8)

--- Script/Get-ProcessAppxPackage.ps1
+++ Script/Get-ProcessAppxPackage.ps1
@@ -1,4 +1,5 @@
-param([switch] $MergeType);
+param([switch] $MergeType,
+	[switch] $All);
 
 begin {
 	$myPath = Split-Path -Parent ($MyInvocation.MyCommand.Path);
@@ -7,22 +8,32 @@ begin {
 	}
 }
 process {
-	$pfn = (.(ScriptDir("\ProcessIdToPackageId.exe")) $_.Id).Split("`t")[1];
+	$pfn = (.(ScriptDir("ProcessIdToPackageId.exe")) $_.Id).Split("`t")[1];
+	$package = $null;
+	$packageState = $null;
+	if ($pfn) {
+		$package = (.(ScriptDir("Get-AppxPackageExt.ps1")) -MergeType:$MergeType) | ?{ $_.PackageFullName -match $pfn };
+		$packageState = (.(ScriptDir("PackageExecutionState.exe")) /get $pfn);
+	}
+
 	$process = $_;
 	$outputObject = $_;
 	if (!$MergeType) {
 		$outputObject = New-Object PSObject `
-			| Add-Member Id $process.Id `
-			| Add-Member ProcessName $process.ProcessName `
-			| Add-Member Process $process `
+			| Add-Member Id $process.Id -PassThru `
+			| Add-Member ProcessName $process.ProcessName -PassThru `
+			| Add-Member Process $process -PassThru `
 		;
 	}
 
 	$outputObject `
-		| Add-Member PackageFullName $pfn `
-		| Add-Member Package (.(ScriptDir("Get-AppxPackageExt.ps1")) -MergeType:$MergeType $pfn) `
+		| Add-Member PackageFullName $pfn -PassThru `
+		| Add-Member Package $package -PassThru `
+		| Add-Member State $packageState `
 	;
 
-	$outputObject;
+	if ($All -or $package) {
+		$outputObject;
+	}
 }
 end {}
