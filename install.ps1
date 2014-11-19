49b1fcc65aa4dfa7897e5d647b730f2ddb71349c
David Risney <dave@deletethis.net>
2014-11-18 19:38:20 -0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,40 +2,45 @@ param(
     $InstallPath = "~\bin",
     $Force);
 
-# Test if it already exists
-if ((Test-Path ($InstallPath + "\Launch-AppxPackage.ps1")) -and !$Force) {
-    throw "AppxUtilities already installed.";
+# Make install directory
+if (!(Test-Path $InstallPath)) {
+    "Creating install path: "  + $InstallPath;
+    mkdir $InstallPath;
 }
 else {
-    # Make install directory
-    if (!(Test-Path $InstallPath)) {
-        mkdir $InstallPath;
-    }
-    $InstallPath = gi $InstallPath;
-
-    # Update PATH to include install directory (if it doesn't already)
-    $path = [Environment]::GetEnvironmentVariable("PATH", "User");
-    $path = $path.Split(";");
-    if (!($path | ?{ $_ -match $InstallPath.FullName })) {
-        $path += @($InstallPath.FullName);
-        $path = $path -join ";";
-        [Environment]::SetEnvironmentVariable("PATH", $path, "User");
-    }
-
-    pushd $InstallPath;
-
-    # Download zip into install directory
-    $client = (New-Object Net.WebClient)
-    $client.Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
-    $client.DownloadFile("https://david-risney.github.io/AppxUtilities/AppxUtilities.zip", $InstallPath + "\AppxUtilities.zip")
-    $zipFile = gi ($InstallPath + "\AppxUtilities.zip");
-
-    # Extract zip to install directory
-    [Reflection.Assembly]::LoadWithPartialName( "System.IO.Compression.FileSystem" ) | Out-Null;
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipFile.FullName, $InstallPath);
-
-    # Delete zip
-    rm AppxUtilities.zip;
-
-    popd;
+    "Install path already exists: "  + $InstallPath;
 }
+$InstallPath = gi $InstallPath;
+
+# Update PATH to include install directory (if it doesn't already)
+$path = [Environment]::GetEnvironmentVariable("PATH", "User");
+if (!$path) {
+    $path = "";
+}
+$path = $path.Split(";");
+if (!($path | ?{ $_ -eq $InstallPath.FullName })) {
+    $path += @($InstallPath.FullName);
+    $path = $path -join ";";
+    [Environment]::SetEnvironmentVariable("PATH", $path, "User");
+    "PATH env variable updated to include install path."
+}
+else {
+    "PATH env variable already contains install path."
+}
+
+pushd $InstallPath;
+
+# Download zip into install directory
+$client = (New-Object Net.WebClient);
+$client.Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials;
+$client.DownloadFile("https://david-risney.github.io/AppxUtilities/AppxUtilities.zip", $InstallPath.FullName + "\AppxUtilities.zip");
+$zipFile = gi ($InstallPath.FullName + "\AppxUtilities.zip");
+
+# Extract zip to install directory
+[Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null;
+[System.IO.Compression.ZipFile]::ExtractToDirectory($zipFile.FullName, $InstallPath.FullName);
+
+# Delete zip
+rm AppxUtilities.zip;
+
+popd;
